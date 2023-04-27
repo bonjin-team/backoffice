f4d974493ed2b977b0557494814dd5c257b13c64
bonjin <bonjin.app@gmail.com>
2023-04-27 16:46:49 +0900

    Add Login, Main Screen

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: 1000,
+        height: 1000,
+      ),
+    );
+  }
+}
